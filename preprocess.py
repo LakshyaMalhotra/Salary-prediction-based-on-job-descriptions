eca1efd7d3316c6531c7282067844c67cddd2233
LakshyaMalhotra <lakshya9009@gmail.com>
2021-01-03 09:24:10 -0800

    Added class for data loading and preprocessing.

--- preprocess.py
+++ preprocess.py
@@ -2,99 +2,129 @@ __author__ = "Lakshya Malhotra"
 __copyright__ = "Copyright (c) 2020 Lakshya Malhotra"
 
 # Import the libraries
+
 import os
-
 import pandas as pd
-from sklearn import model_selection
-
-def read_file(file: str) -> pd.DataFrame:
-    """
-    Read the csv file containing the data.
-    :return: (pd.DataFrame)
-    """
-    return pd.read_csv(file)
+from sklearn import preprocessing
+from typing import List
 
 
-def combine_data(df1: pd.DataFrame, df2: pd.DataFrame, key: str = None) -> pd.DataFrame:
-    """
-    Combine two dataframes based on a common key
-    :param df1: dataframe containing features
-    :param df2: dataframe containing target
-    :param key: common key on which the dataframes are joined
+class Data:
+    def __init__(
+        self,
+        train_feature_file: str,
+        train_target_file: str,
+        test_file: str,
+        cat_vars: List[str],
+        target_var: str,
+        unique_var: str,
+    ):
+        """Data class to load and preprocess the data.
 
-    :return: pd.DataFrame
-    """
-    # check if both dataframes have same rows
-    df = None
-    try:
-        features_key = set(df1[key].values)
-        target_key = set(df2[key].values)
-        assert len(features_key.intersection(target_key)) == len(df1)
-        df = df1.set_index(key).join(df2.set_index(key)).reset_index()
-    except AssertionError:
-        print("Could not join dataframes with different number of rows...exiting!")
-    return df
+        Args:
+            train_feature_file (str): Path to the training features
+            train_target_file (str): Path to the training target
+            test_file (str): Path to the test data
+            cat_vars (list(str)): list of the names of categorical columns
+            target_var (str): target variable (label)
+            unique_var (str): Unique identifier for each training example
+
+        """
+        self.test_file = test_file
+        self.cat_vars = cat_vars
+        self.target_var = target_var
+        self.unique_var = unique_var
+        self.label_encoders = {}
+        self.train_df = self._create_train_df(
+            train_feature_file, train_target_file
+        )
+        self.test_df = self._create_test_df(test_file)
+
+    def _create_train_df(
+        self,
+        train_feature_file: str,
+        train_target_file: str,
+        preprocess=True,
+        label_encode=True,
+    ) -> pd.DataFrame:
+        """
+        Create and preprocess training data.
+        """
+        feature_df = self._load_data(train_feature_file)
+        target_df = self._load_data(train_target_file)
+        train_df = self._concat_dfs(feature_df, target_df)
+        if preprocess:
+            train_df = self._clean_data(
+                train_df, self.unique_var, self.target_var
+            )
+            train_df = self._shuffle_data(train_df)
+        if label_encode:
+            self.label_encode_df(train_df, self.cat_vars)
+
+        return train_df
+
+    def _create_test_df(
+        self, test_file: str, label_encode=True
+    ) -> pd.DataFrame:
+        """
+        Create and preprocess test data.
+        """
+        test_df = self._load_data(test_file)
+        if label_encode:
+            self.label_encode_df(test_df, self.cat_vars)
+
+        return test_df
+
+    def label_encode_df(self, df: pd.DataFrame, cols: list) -> None:
+        for col in cols:
+            if col in self.label_encoders:
+                self._label_encode(df, col, self.label_encoders[col])
+            else:
+                self._label_encode(df, col)
+
+    def _label_encode(
+        self, df: pd.DataFrame, col: str, le: dict = None
+    ) -> None:
+        """
+        Label encode the dataframe.
+        """
+        if le:
+            df[col] = le.transform(df[col])
+
+        else:
+            le = preprocessing.LabelEncoder()
+            le.fit(df[col])
+            df[col] = le.transform(df[col])
+            self.label_encoders[col] = le
+
+    def _load_data(self, file_path: str) -> pd.DataFrame:
+        """Load the data from a file.
+        """
+        df = pd.read_csv(file_path)
+        return df
+
+    def _concat_dfs(
+        self, feature_df: pd.DataFrame, target_df: pd.DataFrame
+    ) -> pd.DataFrame:
+        """Join both feature and target dataframes.
+        """
+        return pd.concat([feature_df, target_df], axis=1)
+
+    def _clean_data(
+        self, df: pd.DataFrame, unique_var: str, target_var: str
+    ) -> pd.DataFrame:
+        """Clean the data by removing duplicates and remove rows with negative salary.
+        """
+        df = df.drop_duplicates(subset=unique_var)
+        df = df[df[target_var] > 0]
+        return df
+
+    def _shuffle_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Shuffle the data.
+        """
+        return df.sample(frac=1).reset_index(drop=True)
 
 
-def clean_data(df: pd.DataFrame, col: str) -> pd.DataFrame:
-    """
-    Cleans the dataframe by dropping duplicates and rows with zero salaries
-    :param df: raw dataframe
-    :param col: column to be used for removing duplicates
-    :return: clean dataframe
-    """
-    clean_df = df.drop_duplicates(subset=col)
-    clean_df = clean_df[clean_df["salary"] > 0]
-
-    assert isinstance(clean_df, pd.DataFrame)
-    return clean_df
-
-def create_folds(df: pd.DataFrame)->pd.DataFrame:
-    """
-    Create folds for cross-validation
-    :param df: Original cleaned dataframe
-    :return: pd.DataFrame
-    """
-    # create a new column and fill it with -1
-    df['kfold'] = -1
-
-    # shuffle all the rows of the dataframe
-    df = df.sample(frac=1).reset_index(drop=True)
-
-    # instantiate the kfold cross validation
-    kf = model_selection.KFold(n_splits=10)
-
-    # fill the new kfold column
-    for fold, (t_, v_) in enumerate(kf.split(X=df)):
-        df.loc[v_, 'kfold'] = fold
-
-    return df
-
-
-if __name__ == "__main__":
-    # define path to the data directory
-    path = "data/"
-    features_file = os.path.join(path, "train_features.csv")
-    target_file = os.path.join(path, "train_salaries.csv")
-
-    # get the features and target dataframes
-    features_df = read_file(features_file)
-    target_df = read_file(target_file)
-
-    # combine both features and target dataframe
-    train_df = combine_data(features_df, target_df, key="jobId")
-
-    if train_df is None:
-        raise TypeError("Dataframe is empty")
-    print(train_df.head())
-
-    # get the cleaned dataframe
-    train_df = clean_data(train_df, col="jobId")
-
-    # create folds in the dataframe
-    train_df = create_folds(train_df)
-    print(train_df.shape)
-    print(train_df.kfold.value_counts())
-    
-    # save this dataframe to another file
-    train_df.to_csv(os.path.join(path, 'train_folds.csv'), index=False)
+class EngineerFeatures:
+    def __init__(self, data):
+        pass
