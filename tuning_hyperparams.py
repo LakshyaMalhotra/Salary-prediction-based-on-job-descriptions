92e0aecf437824ab9bb92f9a6e20db73f39dd146
LakshyaMalhotra <lakshya9009@gmail.com>
2021-01-05 16:40:16 -0800

    Added random forest to hyperparam tuning.

--- tuning_hyperparams.py
+++ tuning_hyperparams.py
@@ -1,8 +1,10 @@
 import os
 import json
 
-from sklearn import metrics
-from sklearn import model_selection
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.ensemble import RandomForestRegressor
 import optuna
 import lightgbm as lgb
 
@@ -23,33 +25,56 @@ class Optimize:
         X = Optimize.train_df.loc[:, features]
         y = Optimize.train_df.salary
 
-        X_train, X_valid, y_train, y_valid = model_selection.train_test_split(
+        X_train, X_valid, y_train, y_valid = train_test_split(
             X, y, test_size=0.2, random_state=23
         )
-        d_train = lgb.Dataset(X_train, label=y_train)
-        params = {
-            "application": "regression",
-            "metric": "mean_squared_error",
-            "verbosity": -1,
-            "lambda_l1": trial.suggest_float("lambda_l1", 1e-8, 10.0, log=True),
-            "lambda_l2": trial.suggest_float("lambda_l2", 1e-8, 10.0, log=True),
-            "num_leaves": trial.suggest_int("num_leaves", 100, 300),
-            "max_depth": trial.suggest_int("max_depth", 6, 24),
-            "learning_rate": trial.suggest_float(
-                "learning_rate", 0.01, 1.0, log=True
-            ),
-            "feature_fraction": trial.suggest_float(
-                "feature_fraction", 0.3, 1.0
-            ),
-            "bagging_fraction": trial.suggest_float(
-                "bagging_fraction", 0.4, 1.0
-            ),
-            "bagging_freq": trial.suggest_int("bagging_freq", 1, 7),
-            "min_child_samples": trial.suggest_int("min_child_samples", 5, 100),
-        }
-        lgbm = lgb.train(params, d_train, num_boost_round=150)
-        y_pred = lgbm.predict(X_valid)
-        error = metrics.mean_squared_error(y_valid, y_pred)
+        classifier_name = trial.suggest_categorical(
+            "classifier", ["lgbr", "rf"]
+        )
+        if classifier_name == "lgbr":
+            d_train = lgb.Dataset(X_train, label=y_train)
+            params = {
+                "application": "regression",
+                "metric": "mean_squared_error",
+                "verbosity": -1,
+                "lambda_l1": trial.suggest_float(
+                    "lambda_l1", 1e-8, 10.0, log=True
+                ),
+                "lambda_l2": trial.suggest_float(
+                    "lambda_l2", 1e-8, 10.0, log=True
+                ),
+                "num_leaves": trial.suggest_int("num_leaves", 100, 500),
+                "max_depth": trial.suggest_int("max_depth", 4, 30),
+                "learning_rate": trial.suggest_float(
+                    "learning_rate", 0.01, 1.0, log=True
+                ),
+                "feature_fraction": trial.suggest_float(
+                    "feature_fraction", 0.3, 1.0
+                ),
+                "bagging_fraction": trial.suggest_float(
+                    "bagging_fraction", 0.4, 1.0
+                ),
+                "bagging_freq": trial.suggest_int("bagging_freq", 1, 7),
+                "min_child_samples": trial.suggest_int(
+                    "min_child_samples", 5, 100
+                ),
+            }
+            classifier_obj = lgb.train(params, d_train, num_boost_round=150)
+        else:
+            params = {
+                "n_estimators": trial.suggest_int("n_estimators", 100, 500),
+                "max_depth": trial.suggest_int("max_depth", 4, 30),
+                "max_features": trial.suggest_categorical(
+                    "max_features", ["log2", "sqrt", None]
+                ),
+                "min_samples_split": trial.suggest_int(
+                    "min_samples_split", 2, 10
+                ),
+            }
+            classifier_obj = RandomForestRegressor(**params, n_jobs=-1)
+            classifier_obj.fit(X_train, y_train)
+        y_pred = classifier_obj.predict(X_valid)
+        error = mean_squared_error(y_valid, y_pred)
 
         return error
 
@@ -83,10 +108,10 @@ if __name__ == "__main__":
     opt = Optimize()
 
     study = optuna.create_study(direction="minimize")
-    study.optimize(opt.optimize, n_trials=40)
+    study.optimize(opt.optimize, n_trials=10)
     best_params_ = study.best_params
     hyperparams_dict = json.dumps(best_params_)
-    with open("models/lgbm_best_hyperparams.json", "w") as f:
+    with open("models/best_hyperparams.json", "w") as f:
         f.write(hyperparams_dict)
 
     print(f"Best parameters: \n{best_params_}")
